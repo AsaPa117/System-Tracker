65e5f4895f8ea83d3381980d22a20f0595ff7961
AsaPa117 <ashton.palma@wsu.edu>
2026-04-26 18:47:15 -0700

    Refactor ReadProcStat for optimized CPU usage tracking
    Refactor ReadProcStat function to improve CPU usage calculation. Removed redundant file reads and added static variables to track previous CPU states.

--- ReadProcStat.c
+++ ReadProcStat.c
@@ -22,46 +22,47 @@ void initCPUData(cpuData *CPU, FILE *file);
 
 double ReadProcStat() 
 {
-    cpuData cpu;
-    cpuData cpuNext; 
+    static unsigned long long prev_idle = 0, prev_total = 0; 
 
     FILE *file = fopen("/proc/stat", "r");
-
     if (file == NULL) 
     {
         printf("Failed to open stat info");
         return 0; //return to indicate failure
-    }
-        
-    initCPUData(&cpu, file); 
-    fclose(file);    
+    } 
 
-    unsigned long long idle0 = cpu.idle + cpu.iowait;
-    unsigned long long nonIdle0 = cpu.user + cpu.nice + cpu.system + cpu.irq + cpu.softirq + cpu.steal;
-    unsigned long long total0 = idle0 + nonIdle0;
-
-    //Delay to read the next line of the stat info to get the next cpu usage data
-    sleep(1);
     FILE *fileNext = fopen("/proc/stat", "r");
     if (fileNext == NULL) 
     {
         printf("Failed to open stat info");
         return 1; //return to indicate failure
     }
-        
-    initCPUData(&cpuNext, fileNext); 
-    fclose(fileNext);    
-        
-    unsigned long long idle1 = cpuNext.idle + cpuNext.iowait;
-    unsigned long long nonIdle1 = cpuNext.user + cpuNext.nice + cpuNext.system + cpuNext.irq + cpuNext.softirq + cpuNext.steal;
-    unsigned long long total1 = idle1 + nonIdle1;
 
-    unsigned long long totald = total1 - total0;
-    unsigned long long idled = idle1 - idle0;
+     cpuData cpu;
+    initCPUData(&cpu, file); 
+    fclose(file);   
+
+    unsigned long long idle = cpu.idle + cpu.iowait; 
+    unsigned long long total = idle + cpu.user + cpu.nice + cpu.system + cpu.irq + cpu.softirq + cpu.steal; 
+    
+    if(prev_total == 0)
+    {
+    prev_total = total; 
+    prev_idle = idle; 
+    return 0.0; 
+    } 
+    
+    unsigned long long totald = total - prev_total; 
+    unsigned long long idled = idle - prev_idle; 
+
+    prev_total = total; 
+    prev_idle = idle; 
+
+    if(totald == 0) {
+        return 0.0; 
+    }
 
     double Percentage = (double)(totald - idled) / totald * 100;
-
-   
     return Percentage; //return to indicate success
 }
 
@@ -69,9 +70,6 @@ void initCPUData(cpuData *CPU, FILE *file)
 {
     char line[500];
     fgets(line, sizeof(line), file); // For now we'll read first line since it contains the overall cpu core usage
-    //while (fgets(line, sizeof(line), file)) {} (We'll use it if we want to read each line for each cpu core usage)
-
-    //printf("Initial Run: %s", line); // Print line of the stat info
         
     // Since each field is raw data, we need to parse to determine CPU Usage. (Idle = idle + iowait) (nonIdle = user + nice + system + irq + softirq + steal) (total = idle + nonIdle)
     char *token = strtok(line, " "); 
